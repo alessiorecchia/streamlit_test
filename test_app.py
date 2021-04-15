156f18018f138ec22a5136ad1933766e0a7a279b
alessiorecchia <recchia.alessio@yahoo.com>
2021-04-15 15:05:09 +0200

    modified test

--- test_app.py
+++ test_app.py
@@ -113,6 +113,7 @@ If you look at the code down here, the dataset is loaded using pandas as you're 
 In this example, we will use a csv file created scraping from IMDb last time.""")
 
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("500_rows.csv")
 df['minmax_norm_ratings'] = 1 + (df['avg_rating'] - df['avg_rating'].min()) / (df['avg_rating'].max() - df['avg_rating'].min()) * 9
