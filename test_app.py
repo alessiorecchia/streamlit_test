306f76c530a413c8ad098043d5c0e5c70ad31259
alessiorecchia <recchia.alessio@yahoo.com>
2021-04-15 14:56:21 +0200

    modified test

--- test_app.py
+++ test_app.py
@@ -132,11 +132,44 @@ st.dataframe(filtered[columns_to_show])
 
 
 
-st.subheader("Visualization with Plotly")
-import plotly.express as px
-fig1 = px.bar(df, x = 'author', y = 'reviews',title = 'very silly test plot')
+st.subheader("Visualization with Matplotlib")
 
-st.plotly_chart(fig1)
+import matplotlib.pyplot as plt
+
+def plot_norm(not_norm_series, norm_series, norm_type: str, bins_ = 20, not_norm_color='#e67e4d', norm_color='#e67eff', edgecolor_='black'):
+    
+    # defining some useful variables
+    norm_min = round(norm_series.min(), 2)
+    norm_max = round(norm_series.max(), 2)
+
+    
+    # Set the number of subplots and the fig dimensions
+    fig, ax = plt.subplots(1, 2, figsize = (15,7))
+
+    # defining the not-normilized graph
+    ax[0].hist(not_norm_series, bins=bins_, color=not_norm_color, histtype='bar', edgecolor=edgecolor_)
+    ax[0].set_title("Not normalized")
+    ax[0].set_xlabel('Ratings')
+    ax[0].set_ylabel('Frequency')
+
+    # defining the normilized graph
+    ax[1].hist(norm_series, bins=bins_, color=norm_color, histtype='bar', edgecolor=edgecolor_)
+    ax[1].set_title(norm_type)
+    ax[1].set_xlabel(f'Ratings (normalized {norm_min} to {norm_max})')
+    ax[1].set_ylabel('Frequency')
+
+    # Set the ticks for x axis in normalized graph, according to the data range
+    plt.sca(ax[1])
+    plt.xticks(np.arange(norm_min, norm_max + 1, 1))
+
+# Examples
+plot_norm(df.avg_rating, df.minmax_norm_ratings, norm_type='Min-Max normalization')
+plot_norm(df.avg_rating, df.mean_norm_ratings, norm_type='Mean normalization')
+
+# import plotly.express as px
+# fig1 = px.bar(df, x = 'author', y = 'reviews',title = 'very silly test plot')
+
+# st.plotly_chart(fig1)
 
 # st.header("Conclusions")
 # st.markdown("""Here you have seen a couple of things you can do using Streamlit.
