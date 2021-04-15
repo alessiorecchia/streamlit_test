5defa6da267e14ec32392c1eef18663af2e8c240
alessiorecchia <recchia.alessio@yahoo.com>
2021-04-15 12:19:15 +0200

    uploaded files needed to deploy web app

--- test_app.py
+++ test_app.py
@@ -4,23 +4,20 @@ import pandas as pd
 logo = Image.open("logo.png")
 st.sidebar.image(logo, width=100)
 # Text/Title
-st.title("This is a title")
+st.title("Welcome to demo app")
 
 st.sidebar.header("Sidebar")
-st.sidebar.text("Writing on the sidebar!")
-st.sidebar.markdown("""This is a template for [Streamlit](https://www.streamlit.io/) to publish some results from your first AI Build Week!
-I wrote some examples of things you could do using Streamlit. Go at the end of the document to look for more resources to learn from!""")
+# st.sidebar.text("Writing on the sidebar!")
+# st.sidebar.markdown("""This is a template for [Streamlit](https://www.streamlit.io/) to publish some results from your first AI Build Week!
+# I wrote some examples of things you could do using Streamlit. Go at the end of the document to look for more resources to learn from!""")
 
-# Header/Subheader
-st.header("This is a header")
-st.subheader("This is a subheader")
+# # Header/Subheader
+# st.header("This is a header")
+# st.subheader("This is a subheader")
 
 st.text("This is text")
-st.markdown("""### And here I'm writing in markdown.
-Hello guys, this is a template I built in Streamlit if you want to deploy a web app with what you have done so far.
-I personally think it is pretty cool and straighforward!
-I will leave here some of the commands that you could try and some of the links to make more advance stuff. 
-Take a look at the code of this page to understand how things are implemented.""")
+st.markdown("""### I know, this webapp looks like the Antonio's one. And it is it!
+I hope you guys are better than me in making things look cool (I know you are!). Scroll down to see other stuff I changed!""")
 
 st.markdown("### Display stuff with `st.write(python_code)`")
 hello_world_variable = "Hello World"
@@ -31,7 +28,7 @@ st.button("Click me")
 
 st.markdown("### If statement with buttons")
 if st.button("Click to show spoiler"):
-    st.write("Hello :D")
+    st.write("Bazinga!")
 
 st.markdown("### Success/Info/Warning/Error")
 
@@ -61,7 +58,7 @@ else:
     st.warning("You're Inactive")
 
 # SelectBox
-occupation = st.selectbox("Your occupation", ["Programmer", "Data Scientist", "Striver"])
+occupation = st.selectbox("Your occupation", ["Programmer", "Data Scientist", "Striver", "Trying to survive"])
 st.write("You selected", occupation)
 
 music = st.multiselect("Which music do you like?", ["Rock", "Pop", "EDM", "Electronic", "Classical", "Traditional"])
@@ -137,20 +134,20 @@ fig1 = px.bar(df, x = 'author', y = 'reviews',title = 'very silly test plot')
 
 st.plotly_chart(fig1)
 
-st.header("Conclusions")
-st.markdown("""Here you have seen a couple of things you can do using Streamlit.
-I found the process pretty smooth myself, very intuitive once you know a bit of Python.
-I want to stress a bit the importance of visualizing things: you're going to deal with people, and if you can show 
-stuff, people can trust you faster... and give you a job.
-So I recommend you to take care of building a nice portfolio, to share it on the internet, so that you can be exposed to people that
-are seeking smart guys and gals like you to be hired!
-""")
-st.header("More resources")
+# st.header("Conclusions")
+# st.markdown("""Here you have seen a couple of things you can do using Streamlit.
+# I found the process pretty smooth myself, very intuitive once you know a bit of Python.
+# I want to stress a bit the importance of visualizing things: you're going to deal with people, and if you can show 
+# stuff, people can trust you faster... and give you a job.
+# So I recommend you to take care of building a nice portfolio, to share it on the internet, so that you can be exposed to people that
+# are seeking smart guys and gals like you to be hired!
+# """)
+# st.header("More resources")
 
-st.markdown("""Following a bunch of links to reduce your *Google fatigue*:
-- Streamlit crash course (*not* covering pandas and plotting) https://www.youtube.com/watch?v=_9WiB2PDO7k
-- Streamlit Tutorials Playlist from [Data Professor](https://www.youtube.com/channel/UCV8e2g4IWQqK71bbzGDEI4Q) https://www.youtube.com/watch?v=ZZ4B0QUHuNc
-- Streamlit official documentation https://docs.streamlit.io/
-- Streamlit cheat sheet https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py
-- How to deploy your Streamlit web-app with Heroku https://towardsdatascience.com/from-streamlit-to-heroku-62a655b7319
- """)
+# st.markdown("""Following a bunch of links to reduce your *Google fatigue*:
+# - Streamlit crash course (*not* covering pandas and plotting) https://www.youtube.com/watch?v=_9WiB2PDO7k
+# - Streamlit Tutorials Playlist from [Data Professor](https://www.youtube.com/channel/UCV8e2g4IWQqK71bbzGDEI4Q) https://www.youtube.com/watch?v=ZZ4B0QUHuNc
+# - Streamlit official documentation https://docs.streamlit.io/
+# - Streamlit cheat sheet https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py
+# - How to deploy your Streamlit web-app with Heroku https://towardsdatascience.com/from-streamlit-to-heroku-62a655b7319
+#  """)
